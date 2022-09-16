9fcaaca858ec50a9b7fdafd9f2f647ba215ae089
alexandermehks <alexandermehks@gmail.com>
2022-09-16 20:56:51 +0200

    first commit

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+	<p> HEj </p>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
