b738740656e9ac95927d1cc3a2d6a691064f3f22
Alexander Mehks <54999803+alexandermehks@users.noreply.github.com>
2022-09-17 14:03:39 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <LandingPage/>
+  </div>
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+	LandingPage
+    
+  }
+}
+</script>
+
+<style>
+	body{
+		background-color: rgb(60, 60, 60);
+	}
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<Navbar msg="Hej"/>
+	</div>
+</template>
+
+<script>
+import Navbar from './Navbar.vue'
+
+export default {
+  name: 'LandingPage',
+  components: {
+	Navbar	
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
