ad82bd7377be07415face72d5c2b38e2c77a89fe
alexandermehks <alexandermehks@gmail.com>
2022-09-22 20:05:20 +0200

    Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,7 @@
 			<a href="/"><v-btn color="rgb(67,63,63)">Home</v-btn></a>
 			<a href="/"><v-btn color="rgb(67,63,63)">Projects</v-btn></a>
 			<a href="/"><v-btn color="rgb(67,63,63)">Blog</v-btn></a>
-			<a href="about"><v-btn color="rgb(67,63,63)">About</v-btn></a>
+			<a href="/https:/youtube.com"><v-btn color="rgb(67,63,63)">About</v-btn></a>
 			<a href="/"><v-btn color="rgb(67,63,63)">Contact</v-btn></a>
 		</div>
 
