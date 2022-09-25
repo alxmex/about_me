9ae297c05e25b72df2c9bb095f0cf2789e871aef
alexandermehks <alexandermehks@gmail.com>
2022-09-25 15:58:13 +0200

    update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,4 +1,5 @@
 <template>
+	<span>
 		<div id="tho">
 			<router-link to="/"><v-btn color="rgb(67,63,63)">Home</v-btn></router-link>
 			<router-link to="/"><v-btn color="rgb(67,63,63)">Projects</v-btn></router-link>
@@ -6,16 +7,32 @@
 			<router-link to="/about"><v-btn color="rgb(67,63,63)">About</v-btn></router-link>
 			<router-link to="/"><v-btn color="rgb(67,63,63)">Contact</v-btn></router-link>
 		</div>
+	
+		<Slide id="hamburger" right :closeOnNavigation="true">
+			<router-link to="/"><v-btn color="rgb(67,63,63)">Home</v-btn></router-link>
+			<router-link to="/"><v-btn color="rgb(67,63,63)">Projects</v-btn></router-link>
+			<router-link to="/"><v-btn color="rgb(67,63,63)">Blog</v-btn></router-link>
+			<router-link to="/about"><v-btn color="rgb(67,63,63)">About</v-btn></router-link>
+			<router-link to="/"><v-btn color="rgb(67,63,63)">Contact</v-btn></router-link>
+		</Slide>
+
+
+
+	</span>
 
 </template>
 
 <script>
+import { Slide } from 'vue-burger-menu'
 export default {
   name: 'Navbar-m',
   props: { 
 	msg: String
 
-  }
+  },
+  components: {
+Slide
+  } 
 }
 </script>
 
@@ -40,6 +57,29 @@ export default {
 .v-btn{
 	color: #a6a28c;
 	margin:1em;
+}
+
+Slide{
+	background-color: red;
+}
+
+@media screen and (max-width: 753px) {
+	#tho{
+		display: none;
+	}
+	#hamburger{
+		display: inline;
+	}
+}
+
+@media screen and (min-width: 754px) {
+	#tho{
+		display: inline;
+	}
+	#hamburger{
+		display: none;
+	}
+
 }
 
 </style>
