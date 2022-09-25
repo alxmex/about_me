b9a5e882427ad1f03a247b03210a774e842c9d63
alexandermehks <alexandermehks@gmail.com>
2022-09-25 18:14:31 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,9 @@ export default Vue.extend({
 .v-main{
 	background-color: #141516;
 	height: 100%;
+	padding: 0px;
+    margin: 0px;
+    box-sizing: border-box;
 }
 
 @import url('https://fonts.googleapis.com/css2?family=Lato&display=swap');
